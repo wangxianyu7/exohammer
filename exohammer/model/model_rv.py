5191294d81a7207b089d90675bcd7def254810f9
Xian-Yu Wang <Xianyu_Wang@126.com>
2024-04-14 15:55:35 -0400

    Update model_rv.py

--- exohammer/model/model_rv.py
+++ exohammer/model/model_rv.py
@@ -3,7 +3,7 @@
 from ttvfast import ttvfast
 from numpy import array
 from exohammer.utilities import generate_planets
-
+import numpy as np
 
 def model_rv(theta, system):
 	dt = 0.4
@@ -21,5 +21,30 @@ def model_rv(theta, system):
 
 	rv_model = model['rv']
 	rv_model = array(rv_model) * au_per_day
+
+
+
+	# Importing the system parameters
+	fixed_labels = system.fixed_labels
+	fixed_values = system.fixed
+	variable_labels = system.variable_labels
+	orb_elements = []
+
+	for i in range(len(fixed_values)):
+		orb_elements.append({'element': fixed_labels[i],
+								'value': fixed_values[i]})
+	for i in range(len(variable_labels)):
+		orb_elements.append({'element': variable_labels[i],
+								'value': theta[i]})
+
+
+	rv_insts_unique = list(set(system.rvinsts))
+	rv_insts_unique.sort()
+	for i in range(len(rv_insts_unique)):
+		for j in orb_elements:
+			if j['element'] == rv_insts_unique[i] + '_offset':
+				offset = j['value']
+		idx =np. where(array(system.rvinsts) == rv_insts_unique[i])
+		rv_model[idx] = rv_model[idx] + offset
 
 	return mod, epo, rv_model
